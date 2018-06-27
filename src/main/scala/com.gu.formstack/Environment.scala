deab51613d6f6f0ba95453b35afa8b7164f76e77
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-27 14:31:38 +0100

    No conversion

--- src/main/scala/com.gu.formstack/Environment.scala
+++ src/main/scala/com.gu.formstack/Environment.scala
@@ -3,24 +3,18 @@ package com.gu.formstack
 // ------------------------------------------------------------------------
 import cats.effect.Sync
 import cats.syntax.flatMap._
-import scala.collection.JavaConverters._
 // ------------------------------------------------------------------------
 
 class Environment[F[_]] {
   implicit val sync: Sync[F] = Sync[F]
 
-  def getEnv: F[Map[String, String]] = sync.delay {
-    Option(System.getenv).map(_.asScala.toMap)
-  }.flatMap {
-    case None =>
-      Sync[F].raiseError(new RuntimeException("Environment is empty"))
-    case Some(x) =>
-      Sync[F].pure(x)
+  def getEnv: F[java.util.Map[String, String]] = sync.delay {
+    System.getenv
   }
 
   def getToken: F[String] =
-    getEnv.flatMap { env: Map[String, String] =>
-      env.get("OAUTH_TOKEN") match {
+    getEnv.flatMap { env: java.util.Map[String, String] =>
+      Option(env.get("OAUTH_TOKEN")) match {
         case Some(oauth) => Sync[F].pure(oauth)
         case None =>
           Sync[F].raiseError(new RuntimeException("Missing OAUTH_TOKEN"))
