aa32e77157c36479f53181e5b356abc03573bbba
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-27 15:19:18 +0100

    Do not seal stream consumption in IO

--- src/main/scala/com.gu.formstack/StreamOps.scala
+++ src/main/scala/com.gu.formstack/StreamOps.scala
@@ -1,23 +1,21 @@
 package com.gu.formstack
 
 // ------------------------------------------------------------------------
-import cats.effect.Sync
 import java.io.{ InputStream, OutputStream }
 import java.nio.charset.StandardCharsets
 import scala.io.Source
 // ------------------------------------------------------------------------
 
-class StreamOps[F[_]](val logger: LoggingService[F]) {
-  implicit val sync: Sync[F] = Sync[F]
+class StreamOps {
 
-  def consume(is: InputStream): F[String] =
-    sync.bracket(sync.pure(is)) { is =>
-      val contents = Source.fromInputStream(is).mkString
-      sync.pure(contents)
-    }(is => sync.delay(is.close()))
+  def consume(is: InputStream): String = {
+    val contents = Source.fromInputStream(is).mkString
+    is.close()
+    contents
+  }
 
-  def writeAndClose(os: OutputStream, contents: String): F[Unit] =
-    sync.bracket(sync.pure(os)) { os =>
-      sync.delay(os.write(contents.getBytes(StandardCharsets.UTF_8)))
-    }(os => sync.delay(os.close()))
+  def writeAndClose(os: OutputStream, contents: String): Unit = {
+    os.write(contents.getBytes(StandardCharsets.UTF_8))
+    os.close()
+  }
 }
