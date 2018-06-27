6ed59641a449385f38ec1dd2a6dc82100bfb1fd3
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-27 10:23:31 +0100

    Add log4j dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,10 @@
 import sbt._
 
 object Dependencies {
-  val awsVersion = "1.2.0"
+  val awsVersion = "1.1.0"
   val circeVersion = "0.9.3"
   val http4sVersion = "0.18.2"
+  val log4jVersion = "2.8.2"
 
   lazy val circe = Seq(
     "circe-core",
@@ -12,8 +13,14 @@ object Dependencies {
   ).map("io.circe" %% _ % circeVersion)
 
   lazy val aws = Seq(
-    "aws-lambda-java-core"
+    "aws-lambda-java-core",
+    "aws-lambda-java-log4j2"
   ).map("com.amazonaws" % _ % awsVersion)
+
+  lazy val log4j = Seq(
+    "log4j-core",
+    "log4j-api"
+  ).map("org.apache.logging.log4j" % _ % log4jVersion)
 
   lazy val http4s = Seq(
     "http4s-dsl",
