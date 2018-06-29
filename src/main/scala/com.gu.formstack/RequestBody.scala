a178c8dad630012e8c0fb778a1683cf0adc900bb
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-29 11:42:22 +0100

    Decode body inside json payload

--- src/main/scala/com.gu.formstack/RequestBody.scala
+++ src/main/scala/com.gu.formstack/RequestBody.scala
@@ -9,7 +9,11 @@ import io.circe.parser.parse
 class RequestBody(logger: LoggingService) {
 
   def decode(body: String): IO[Json] = IO.suspend {
-    parse(body) match {
+    (for {
+      json <- parse(body)
+      field <- json.hcursor.downField("body").as[String]
+      jsonBody <- parse(field)
+    } yield jsonBody) match {
       case Left(e) =>
         logger.error(s"The payload isn't valid JSON:\n$body", e)
         IO.raiseError(e)
